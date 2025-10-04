359d879370167df7c39a7c549c0b1e6661e793c9
vtn-hz <juaniyvalen1234@gmail.com>
2025-10-04 18:54:22 -0300

    ref: removed migrated attributes

--- src/sistema/gestion/clinica/modelo/Paciente.java
+++ src/sistema/gestion/clinica/modelo/Paciente.java
@@ -2,50 +2,19 @@ package sistema.gestion.clinica.modelo;
 
 import sistema.gestion.clinica.Interfaces.IProtocoloOcuparSala;
 
-import java.time.LocalDateTime;
-
 public abstract class Paciente extends Persona implements IProtocoloOcuparSala  {
 
     private String historiaClinica;
-    private int numeroOrden;
-    private LocalDateTime fechaIngreso;
-
+    
     public Paciente(String NyA, String dni, Domicilio domicilio, String historiaClinica) {
         super(NyA, dni, domicilio);
         this.historiaClinica = historiaClinica;
-        this.fechaIngreso = LocalDateTime.now();
     }
 
     public String getHistoriaClinica() {
         return historiaClinica;
     }
 
-    public LocalDateTime getFechaIngreso() {
-        return fechaIngreso;
-    }
-
-    public int getNumeroOrden() {
-        return numeroOrden;
-    }
-
-    public void setNumeroOrden(int numeroOrden) {
-        this.numeroOrden = numeroOrden;
-    }
-
-    public int getRangoEtario() {
-        // TODO falta implementación
-        return 0;
-    }
-
-    @Override
-    protected String getTipoPersona() {
-        return "Paciente";
-    }
-
-    @Override
-    protected String getInfoEspecialista() {
-        return "Historia Clínica: " + historiaClinica;
-    }
 
     @Override
     public String toString() {
@@ -56,8 +25,6 @@ public abstract class Paciente extends Persona implements IProtocoloOcuparSala  
                 ", ciudad='" + getDomicilio().getCiudad() + '\'' +
                 ", telefono='" + getDomicilio().getTelefono() + '\'' +
                 ", historiaClinica='" + historiaClinica + '\'' +
-                ", numeroOrden=" + numeroOrden +
-                ", fechaIngreso=" + fechaIngreso +
                 '}';
     }
 }
