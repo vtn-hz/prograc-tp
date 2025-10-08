fa7bc582ee19d035efdb4f6145291c9cc2dc02c8
vickychere <vickychere10@gmail.com>
2025-10-07 22:56:41 -0300

    Feature HabitacionFactory

---
+++ src/mdp/ingenieria/clinicagestion/model/clinica/habitacion/HabitacionFactory.java
@@ -0,0 +1,35 @@
+package mdp.ingenieria.clinicagestion.model.clinica.habitacion;
+
+import mdp.ingenieria.clinicagestion.model.clinica.Habitacion;
+
+/**
+ * Fábrica de objetos Habitacion.
+ */
+public class HabitacionFactory {
+    public static final String HABITACION_COMPARTIDA = "compartida";
+    public static final String HABITACION_PRIVADA = "privada";
+    public static final String HABITACION_TERAPIA_INTENSIVA = "terapia_intensiva";
+
+    /**
+     * Crea una habitación del tipo indicado.
+     *
+     * <b>pre:</b> tipo no es nulo ni vacío; el tipo existe; costo asigancion >= 0; costo adicional >= 0 <br>
+     * <b>post:</b> devuelve una instancia de Habitacion del tipo solicitado.
+     *
+     * @param tipo                  identificador textual del tipo de habitación
+     * @param costoAsignacion       costo base por asignación
+     * @param costoAdicional        costo adicional (interpretación según el tipo concreto)
+     * @return instancia concreta de Habitacion
+     */
+    public Habitacion create(String tipo, double costoAsignacion, double costoAdicional) {
+        Habitacion habitacion = null;
+
+        switch(tipo){
+            case HABITACION_COMPARTIDA: habitacion = new HabitacionCompartida(costoAsignacion,costoAdicional); break;
+            case HABITACION_PRIVADA: habitacion = new HabitacionPrivada(costoAsignacion,costoAdicional); break;
+            case HABITACION_TERAPIA_INTENSIVA: habitacion = new HabitacionTerapiaIntensiva(costoAsignacion,costoAdicional); break;
+        }
+
+        return habitacion;
+    }
+}
