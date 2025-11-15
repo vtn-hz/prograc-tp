325cf993979d0fa36ecf0fef2cf8d577fc4c256c
sim-schleider <simschleider@gmail.com>
2025-11-15 12:14:10 -0300

    feat: better asociados generator

--- src/mdp/ingenieria/clinicagestion/controller/AsociadoGenerator.java
+++ src/mdp/ingenieria/clinicagestion/controller/AsociadoGenerator.java
@@ -7,31 +7,39 @@ import java.util.List;
 import java.util.Random;
 
 public class AsociadoGenerator {
+    private final Random random = new Random();
+
     private static final String[] NAMES = {
-            "Juan", "María", "Carlos", "Lucía", "Pedro", "Sofía", "Javier", "Ana"
+            "Juan", "María", "Carlos", "Lucía", "Pedro", "Sofía", "Javier", "Ana",
+            "Diego", "Valentina", "Agustín", "Victoria", "Nicolás", "Camila",
+            "Martín", "Julieta", "Facundo", "Florencia", "Tomás", "Milagros",
+            "Emilia", "Mateo", "Lautaro", "Abril"
     };
 
     private static final String[] SURNAMES = {
-            "Gómez", "Fernández", "Rodríguez", "Pérez", "López", "Martínez", "Sánchez", "Ramírez"
+            "Gómez", "Fernández", "Rodríguez", "Pérez", "López", "Martínez",
+            "Sánchez", "Ramírez", "Torres", "Álvarez", "Romero", "Suárez",
+            "Herrera", "Aguirre", "Molina", "Silva", "Ortiz", "Benítez",
+            "Castro", "Domínguez"
     };
 
     private static final String[] CITIES = {
-            "Buenos Aires", "Córdoba", "Rosario", "Mendoza", "La Plata", "Salta", "Tucumán", "Mar del Plata"
+            "Buenos Aires", "Córdoba", "Rosario", "Mendoza", "La Plata", "Salta",
+            "Tucumán", "Mar del Plata", "Santa Fe", "Resistencia", "Corrientes",
+            "Neuquén", "San Juan", "San Luis", "Posadas", "Bahía Blanca",
+            "Paraná", "Rawson", "Bariloche"
     };
 
-    private static final String[] ADDRESSES = {
-            "Av. Luro 742", "Calle Falsa 123", "San Martín 450", "Belgrano 999",
-            "Mitre 210", "Rivadavia 330", "9 de Julio 1200", "Independencia 678"
+    private static final String[] STREET_NAMES = {
+            "Av. Luro", "San Martín", "Belgrano", "Mitre",
+            "Rivadavia", "9 de Julio", "Independencia", "Sarmiento",
+            "Hipólito Yrigoyen", "Las Heras", "Perón", "Lavalle",
+            "Pueyrredón", "Dorrego", "Catamarca", "Jujuy", "La Rioja"
     };
 
-    private final Random random = new Random();
-
-    private String randomPhone() {
-        return String.format("%03d %03d-%04d",
-                random.nextInt(1000),
-                random.nextInt(1000),
-                random.nextInt(10000));
-    }
+    private static final String[] AR_AREA_CODES = {
+            "11", "221", "223", "261", "341", "351", "381", "299", "387", "376"
+    };
 
     private String randomDni() {
         return String.format("%08d", random.nextInt(100_000_000));
@@ -45,12 +53,21 @@ public class AsociadoGenerator {
         return SURNAMES[random.nextInt(SURNAMES.length)];
     }
 
+    private String randomPhone() {
+        String area = AR_AREA_CODES[random.nextInt(AR_AREA_CODES.length)];
+        int first = 100 + random.nextInt(900);
+        int second = 1000 + random.nextInt(9000);
+        return area + " " + first + "-" + second;
+    }
+
     private String randomCity() {
         return CITIES[random.nextInt(CITIES.length)];
     }
 
     private String randomAddress() {
-        return ADDRESSES[random.nextInt(ADDRESSES.length)];
+        String street = STREET_NAMES[random.nextInt(STREET_NAMES.length)];
+        int number = 1 + random.nextInt(4000);
+        return street + " " + number;
     }
 
     public String[] generateFields() {
